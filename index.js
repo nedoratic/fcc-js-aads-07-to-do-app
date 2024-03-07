66a343d4f8a7a128562c63ce1f7bc4ec42ee69f4
nedoratic <nedoratic@gmail.com>
2024-03-07 21:23:31 +0100

    edit task function added

--- index.js
+++ index.js
@@ -62,3 +62,18 @@ const deleteTask = (buttonEl) => {
 	taskData.splice(dataArrIndex, 1);
 	localStorage.setItem('data', JSON.stringify(taskData));
 };
+
+// Edit Task
+const editTask = (buttonEl) => {
+	const dataArrIndex = taskData.findIndex((item) => item.id === buttonEl.parentElement.id);
+
+	currentTask = taskData[dataArrIndex];
+
+	titleInput.value = currentTask.title;
+	dateInput.value = currentTask.date;
+	descriptionInput.value = currentTask.description;
+
+	addOrUpdateTaskBtn.innerText = 'Update Task';
+
+	taskForm.classList.toggle('hidden');
+};
